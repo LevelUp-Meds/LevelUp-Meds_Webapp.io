b2efc2fe4addafe6f36192b593bcf836673039e3
Corey Fortin <coreyhfortin@gmail.com>
2022-10-31 09:36:41 -0400

    Updated Menubar For Double Stacked
    When screen is smaller the menu buttons can stack in neat rows to save space. Also, buttons are standardized size

--- levelup-meds/src/components/Menubar/menubar.js
+++ levelup-meds/src/components/Menubar/menubar.js
@@ -22,16 +22,16 @@ const menubar = () => (
   <Box sx={{ bgcolor: "#f6e188"}}>
     <AppBar sx={{bgcolor: "transparent", color:"black"}} position="static">
         <Toolbar>
-            <Box sx={{mr: 1}}>
+            <Box sx={{mr: 'auto'}}>
               <Button color="inherit">
                 <img src={LUMLogo} alt="main-logo"></img>
               </Button>
             </Box>
             <Box sx={{mr: 0, ml: 'auto'}}>
-              <Button color="inherit" sx={{ mx: 1, color: "white", bgcolor: "orange"}}>Medications</Button>
-              <Button color="inherit" sx={{ mx: 1, color: "white",  bgcolor: "orange"}}>Calendar</Button>
-              <Button color="inherit" sx={{ mx: 1, color: "white",  bgcolor: "orange"}}>Login</Button>
-              <Button color="inherit" sx={{ mx: 1, color: "white",  bgcolor: "orange"}}>Sign Up</Button>
+              <Button color="inherit" sx={{ width: 150, mr: 1, color: "white", bgcolor: "orange"}}>Medications</Button>
+              <Button color="inherit" sx={{ width: 150, mr: 1, color: "white",  bgcolor: "orange"}}>Calendar</Button>
+              <Button color="inherit" sx={{ width: 150, mr: 1, color: "white",  bgcolor: "orange"}}>Login</Button>
+              <Button color="inherit" sx={{ width: 150, mr: 1, color: "white",  bgcolor: "orange"}}>Sign Up</Button>
             </Box>
         </Toolbar>
     </AppBar>
