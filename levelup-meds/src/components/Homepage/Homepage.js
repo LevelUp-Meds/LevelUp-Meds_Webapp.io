246425e52d0e160d3c1088581ff350eec95883e0
edwardesqueda <esquede@outlook.com>
2022-11-05 23:00:10 -0400

    created firebase config file

--- levelup-meds/src/components/Homepage/Homepage.js
+++ levelup-meds/src/components/Homepage/Homepage.js
@@ -3,9 +3,10 @@ import PropTypes from "prop-types";
 import styles from "./Homepage.module.scss";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import Menubar from "../Menubar/Menubar"
-import Appointment from "../Appointment/Appointment"
+import Menubar from "../Menubar/Menubar";
+import Appointment from "../Appointment/Appointment";
 import { Box } from "@mui/material";
+import firebaseConfig from "../config/firebase";
 
 function Homepage() {
   const navigate = useNavigate();
@@ -14,10 +15,12 @@ function Homepage() {
     navigate("/login");
   };
 
+  console.log(firebaseConfig);
+
   return (
     <Box className={styles.Homepage}>
-      <Menubar/>
-      <Appointment/>
+      <Menubar />
+      <Appointment />
       <Box>
         <h1>HOMEPAGE</h1>
         <Button variant="contained" onClick={goToLoginPage}>
