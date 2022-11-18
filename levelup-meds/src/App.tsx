50c99fb1c6852ffb0c86738dc0379bf60b3770e9
edwardesqueda <esquede@outlook.com>
2022-11-18 11:32:41 -0500

    fixed errors

--- levelup-meds/src/App.tsx
+++ levelup-meds/src/App.tsx
@@ -8,9 +8,11 @@ import ResetPassword from "./components/ResetPassword/ResetPassword";
 import Homepage from "./components/Homepage/Homepage";
 import { AuthContext } from "./components/context/AuthContext";
 import CreateSuccess from "./components/CreateSuccess/CreateSuccess";
+Auth - Feature;
 import ProtectedRoute from "./components/Routes/ProtectedRoute";
 import Calendar from "./components/Calendar/Calendar";
 import Medication from "./components/Medication/Medication";
+main;
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/login" element={<Login />}></Route>
+          Auth-Feature
           <Route
             path="/dashboard"
             element={
@@ -29,7 +32,7 @@ function App() {
           ></Route>
           <Route path="/dashboard" element={<Dashboard />}></Route>
           <Route path="/calendar" element={<Calendar />}></Route>
-          <Route path="/calendar" element={<Calendar/>}></Route> 
+          main
           <Route path="/register" element={<Register />}></Route>
           <Route path="/medication" element={<Medication />}></Route>
           <Route path="/resetpassword" element={<ResetPassword />}></Route>
