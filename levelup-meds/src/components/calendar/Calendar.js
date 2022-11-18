da1a723a489c2382ec7fba50dc909a6318365570
raops01 <raops01@pfw.edu>
2022-11-18 11:27:44 -0500

    test events added

--- levelup-meds/src/components/calendar/Calendar.js
+++ levelup-meds/src/components/calendar/Calendar.js
@@ -3,20 +3,40 @@ import React, {useState} from 'react';
 import {Calendar, momentLocalizer} from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { auth, db} from "../firebase/firebase";
+import {db, auth} from '../firebase/firebase';
+import {collection, doc, query, where, getDocs, onSnapshot, getFirestore} from 'firebase/firestore';
 
 const localizer = momentLocalizer(moment);
+//const db = getFirestore();
+
+const getEvents = async() => {
+
+}
+
+const calEvents = [
+    {
+        start: new Date(2022, 4, 3, 12, 3, 12),
+        end: new Date(2022, 4, 14, 10, 50, 0),
+        title: "Hey! It's Spyro!"
+        //2022, 4, 14, 13, 21, 25
+    },
+    {
+        start: new Date(),
+        end: new Date(),
+        title: "Banjo-Kazooie"
+    }
+]
 
 const LevelUpMedsCalendar = () => {
     return (
        <>
         <Calendar 
-            //events={}
+            events={calEvents}
             localizer={localizer}
             startAccessor="start"
-            endAccessor="end"
-            style={{height:500}}
+            style={{height:700}}
             defaultView='day'
+            defaultDate={moment().toDate()}
          />
        </>
     );
