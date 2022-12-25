d7233e36f311b6815484cd9b420b03df0e77277e
raops01 <raops01@pfw.edu>
2022-12-24 23:45:48 -0500

    adding events to Firebase/calendar working

--- levelup-meds/src/components/Calendar/Calendar.js
+++ levelup-meds/src/components/Calendar/Calendar.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import {useRef} from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { db } from "../firebase/config";
-import { collection, getDocs, Timestamp } from "firebase/firestore";
+import { collection, getDocs, Timestamp, addDoc } from "firebase/firestore";
+import './AddEvents.css'
+import {Button, Badge} from "react-bootstrap";
 
 const localizer = momentLocalizer(moment);
 
@@ -12,6 +14,28 @@ const appointments = collection(db, "Appointments");
 const medications = collection(db, "Medications");
 
 var calEvents = [];
+
+
+const calendarStyle = {
+  height: 940, 
+  width: 1100,
+  position: "relative",
+  float: "left"
+}
+
+const formStyle = {
+  color: "black",
+  backgroundColor: "white",
+  border: "10px solid yellow",
+  borderRadius: "25px",
+  fontSize: "30px",
+  margin: "auto",
+  textAlign: "center",
+  fontFamily: "Montserrat",
+  float: "right",
+  position: "relative"
+}
+
 
 const getMedications = async () => {
   const medSnap = await getDocs(medications);
@@ -45,6 +69,31 @@ getAppointments();
 getMedications();
 
 const LevelUpMedsCalendar = () => {
+
+  const appName = useRef();
+  const appNotes = useRef();
+  const appDate = useRef();
+  const appAddress = useRef();
+
+  const submitHandler = async(event) => {
+   event.preventDefault();
+
+   const appointment = appName.current.value;
+   const appointmentNotes = appNotes.current.value;
+   const appointmentDate = new Date(appDate.current.value);
+   const apppointmentLocation = appAddress.current.value;
+ 
+   const newDoc = await addDoc(appointments, {
+    address: apppointmentLocation,
+    appointmentDate: Timestamp.fromDate(appointmentDate),
+    name: appointment,
+    notes: appointmentNotes
+   })
+
+   window.location.reload(false);
+
+  }
+
   return (
     <>
       <Calendar
@@ -52,10 +101,42 @@ const LevelUpMedsCalendar = () => {
         localizer={localizer}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 700 }}
+        style={calendarStyle}
         defaultView="day"
         defaultDate={moment().toDate()}
       />
+      <br />
+      <br />
+      <div style={formStyle}>
+        <form onSubmit={submitHandler} method="POST">
+          <fieldset>
+            <legend>Add Appointment:</legend>
+          <div>
+            <label>Appointment Name: </label>
+            <input type="text" ref={appName} name="name" size="35" required ></input>
+          </div>
+
+          <div>
+            <label>Address: </label>
+            <input type="text" ref={appAddress} name="name" size="50" required></input>
+          </div>
+          
+          <div>
+            <label>Notes: </label>
+            <input type="text" ref={appNotes} name="app_notes" required ></input>
+          </div>
+
+          <div>
+            <label>Date and Time: </label>
+            <input type="datetime-local" ref={appDate} name="date" required ></input>
+          </div>
+
+          <div>
+            <input type="submit" name="Submit" value="Add to Calendar"></input>
+          </div>
+          </fieldset>
+        </form>
+      </div>
     </>
   );
 };
--- levelup-meds/src/components/Dashboard/Dashboard.js
+++ levelup-meds/src/components/Dashboard/Dashboard.js
@@ -10,7 +10,6 @@ import { useSignup } from "../hooks/useSignup";
 import Medication from "../Medication/Medication";
 import { Box } from "@mui/system";
 import CustomDay from "../Calendar/CustomDay";
-import LevelUpMedsCalendar from "../Calendar/Calendar"
 import Footer from "../Footer/Footer";
 // import { useLogout } from "../hooks/useLogout";
 
@@ -78,7 +77,7 @@ function Dashboard() {
             >{`Welcome, ${user.displayName}!`}</FormLabel>
             {/* <p>User email: {user && user.email}</p> */}
             {/* {user && <p> Name: {user.displayName}</p>} */}
-            <LevelUpMedsCalendar></LevelUpMedsCalendar>
+            <CustomDay></CustomDay>
             {/* <Medication></Medication> */}
             {/* <Button variant="contained" onClick={handleClick}>
               Update User
