ac8b3abbc8b0989088ef22a6c0df4be92e2dba58
esque01 <esquede@outlook.com>
2023-02-22 19:36:33 -0500

    merged earlier auth feature

--- levelup-meds/src/components/Calendar/Calendar.js
+++ levelup-meds/src/components/Calendar/Calendar.js
@@ -3,6 +3,13 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { db } from "../firebase/config";
+import "../../Calendar.css";
+import AddAppointment from "../AddAppointment/AddAppointment";
+import DeleteAppointment from "../DeleteAppointment/DeleteAppointment";
+import DeleteMedication from "../DeleteMedication/DeleteMedication";
+import UpdateAppointment from "../UpdateAppointment/UpdateAppointment";
+import AddMedications from "../AddMedications/AddMedications";
+import UpdateMedications from "../UpdateMedications/UpdateMedications";
 import { collection, getDocs, Timestamp } from "firebase/firestore";
 import Menubar from "../Menubar/Menubar";
 
@@ -11,39 +18,112 @@ const localizer = momentLocalizer(moment);
 const appointments = collection(db, "Appointments");
 const medications = collection(db, "Medications");
 
-var calEvents = [];
-
-const getMedications = async () => {
-  const medSnap = await getDocs(medications);
-
-  medSnap.forEach((doc) => {
-    console.log(doc);
-    let title = doc.data().name;
-    let start = doc.data().time.toDate();
-    let end = doc.data().time.toDate();
-
-    let event = { start, end, title };
-    calEvents.push(event);
-  });
+const calendarStyle = {
+  height: 950,
+  width: 1100,
+  position: "fixed",
+  float: "left",
+  backgroundColor: "white",
+  fontFamily: "Montserrat",
 };
 
-const getAppointments = async () => {
-  const appSnap = await getDocs(appointments);
+const formStyle = {
+  color: "black",
+  backgroundColor: "yellow",
+  border: "5px solid red",
+  borderRadius: "25px",
+  fontSize: "30px",
+  margin: "auto",
+  textAlign: "center",
+  fontFamily: "Montserrat",
+  float: "right",
+  position: "relative",
+};
 
-  appSnap.forEach((doc) => {
-    console.log(doc);
-    let title = doc.data().name;
-    let start = doc.data().appointmentDate.toDate();
-    let end = doc.data().appointmentDate.toDate();
+const LevelUpMedsCalendar = () => {
+  var calEvents = [];
 
-      let info = "Name: " + title + "\nStarts at: " + start + "\nAddress: " + doc.data().address + "\nNotes: " + doc.data().notes
+  const getMedications = async () => {
+    const medSnap = await getDocs(medications);
 
-      let event = { start, end, title, info};
+    medSnap.forEach((doc) => {
+      //console.log(doc);
+      let title = doc.data().name;
+      let start = doc.data().time.toDate();
+      let end = doc.data().time.toDate();
+
+      let info =
+        "Name: " +
+        title +
+        "\nAmount: " +
+        doc.data().amount +
+        "\nNotes: " +
+        doc.data().notes +
+        "\nStart date: " +
+        start +
+        "\nDays to take:\n";
+
+      let days = doc.data().days;
+
+      if (days.m === true) {
+        info += "Monday\n";
+      }
+
+      if (days.t === true) {
+        info += "Tuesday\n";
+      }
+
+      if (days.w === true) {
+        info += "Wednesday\n";
+      }
+
+      if (days.r === true) {
+        info += "Thursday\n";
+      }
+
+      if (days.f === true) {
+        info += "Friday\n";
+      }
+
+      if (days.s === true) {
+        info += "Saturday\n";
+      }
+
+      if (days.u === true) {
+        info += "Sunday\n";
+      }
+
+      let color = "green";
+
+      let event = { start, end, title, info, color };
       calEvents.push(event);
     });
   };
-  
-  
+
+  const getAppointments = async () => {
+    const appSnap = await getDocs(appointments);
+
+    appSnap.forEach((doc) => {
+      //console.log(doc)
+      let title = doc.data().name;
+      let start = doc.data().appointmentDate.toDate();
+      let end = doc.data().appointmentDate.toDate();
+
+      let info =
+        "Name: " +
+        title +
+        "\nStarts at: " +
+        start +
+        "\nAddress: " +
+        doc.data().address +
+        "\nNotes: " +
+        doc.data().notes;
+
+      let event = { start, end, title, info };
+      calEvents.push(event);
+    });
+  };
+
   getAppointments();
   getMedications();
 
@@ -54,10 +134,44 @@ const getAppointments = async () => {
         localizer={localizer}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 700 }}
+        style={calendarStyle}
         defaultView="day"
         defaultDate={moment().toDate()}
+        selectable
+        onSelectEvent={(event) => alert(event.info)}
+        slotPropGetter={() => {
+          return { style: { backgroundColor: "white" } };
+        }}
+        eventPropGetter={(event) => {
+          const backgroundColor = event.color === "green" ? "green" : "blue";
+          return {
+            style: { backgroundColor: backgroundColor, color: "white" },
+          };
+        }}
       />
+
+      <div style={formStyle}>
+        <AddAppointment />
+        <DeleteAppointment />
+        <DeleteMedication />
+        <UpdateAppointment />
+        <AddMedications />
+        <UpdateMedications />
+      </div>
+
+      <Menubar></Menubar>
+
+      <>
+        <Calendar
+          events={calEvents}
+          localizer={localizer}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 700 }}
+          defaultView="day"
+          defaultDate={moment().toDate()}
+        />
+      </>
     </>
   );
 };
