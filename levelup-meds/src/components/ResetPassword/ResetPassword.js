dae3c3d3462a9815a24f2e0529f435e58a8967f9
Edward Esqueda <esquede@outlook.com>
2023-02-22 21:19:51 -0500

    removed map

--- levelup-meds/src/components/ResetPassword/ResetPassword.js
+++ levelup-meds/src/components/ResetPassword/ResetPassword.js
@@ -1,11 +1,6 @@
 import React from "react";
-import Map from "../Map/Map";
 import styles from "./ResetPassword.module.scss";
 
-const ResetPassword = () => (
-  <div className={styles.ResetPassword}>
-    <Inbox></Inbox>
-  </div>
-);
+const ResetPassword = () => <div className={styles.ResetPassword}></div>;
 
 export default ResetPassword;
