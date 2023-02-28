d4a0033714f09ac88bd278a6d46a5294cf567520
raops01 <raops01@pfw.edu>
2023-02-28 00:31:15 -0500

    only get events from signed in user

--- levelup-meds/src/components/Calendar/Calendar.js
+++ levelup-meds/src/components/Calendar/Calendar.js
@@ -4,44 +4,56 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import db from "../database/FirestoreConfig";
-import { collection, getDocs, Timestamp } from "firebase/firestore";
+import auth from "../Auth/AuthProvider";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import Menubar from "../Menubar/Menubar";
+import { onAuthStateChanged } from "firebase/auth";
 
 const localizer = momentLocalizer(moment);
 
 const appointments = collection(db, "Appointments");
 const medications = collection(db, "Medications");
-
+// const loggedInUser = auth.currentUser;
+//console.log(loggedInUser)
 var calEvents = [];
 
-const getMedications = async () => {
-  const medSnap = await getDocs(medications);
 
-  medSnap.forEach((doc) => {
+const getMedications = async(loggedInUser) => {
+
+    const q = query(medications, where('profileID', '==', "/Profiles/" + loggedInUser.uid))
+    const medSnap = await getDocs(q);
+
+    medSnap.forEach((doc) => {
     let title = doc.data().name;
     let start = doc.data().time.toDate();
     let end = doc.data().time.toDate();
-
     let event = { start, end, title };
     calEvents.push(event);
   });
+  
 };
 
-const getAppointments = async () => {
-  const appSnap = await getDocs(appointments);
+const getAppointments = async(loggedInUser) => {
 
-  appSnap.forEach((doc) => {
-    let title = doc.data().name;
-    let start = doc.data().appointmentDate.toDate();
-    let end = doc.data().appointmentDate.toDate();
-
-    let event = { start, end, title };
-    calEvents.push(event);
-  });
+    const q = query(appointments, where('profileID', '==', "/Profiles/" + loggedInUser.uid))
+    const appSnap = await getDocs(q);
+  
+    appSnap.forEach((doc) => {
+      let title = doc.data().name;
+      let start = doc.data().appointmentDate.toDate();
+      let end = doc.data().appointmentDate.toDate();
+      let event = { start, end, title };
+      calEvents.push(event);
+    });
 };
-
-getAppointments();
-getMedications();
+ 
+onAuthStateChanged(auth, (user) => {
+  if(user)
+  {
+    getAppointments(user)
+    getMedications(user)
+  }
+})
 
 const LevelUpMedsCalendar = () => {
   return (
