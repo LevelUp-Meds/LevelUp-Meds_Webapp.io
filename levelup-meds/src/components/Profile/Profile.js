7392e57a4eb9256bb27f1151c40d0b6c166d887d
raops01 <raops01@pfw.edu>
2023-03-11 18:19:47 -0500

    updating profile info correctly

--- levelup-meds/src/components/Profile/Profile.js
+++ levelup-meds/src/components/Profile/Profile.js
@@ -1,34 +1,27 @@
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Divider from "@mui/material/Divider";
-import Avatar from "@mui/material/Avatar";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import PlaceIcon from "@mui/icons-material/Place";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import CreateIcon from "@mui/icons-material/Create";
+import Input from "@mui/material/Input";
 import Menubar from "../Menubar/Menubar";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
 import "../../App.css";
-import { CardHeader } from "@mui/material";
+import { Button, CardHeader, FormControlLabel, getDialogContentTextUtilityClass } from "@mui/material";
 import db  from "../database/FirestoreConfig";
 import auth from "../Auth/AuthProvider";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, getDoc, doc, updateDoc } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 import {useState, React} from "react";
 import TexttoSpeech from "../TextToSpeech/TextToSpeech";
 
 const users = collection(db, "Profiles");
-const appointments = collection(db, "Appointments");
-const medications = collection(db, "Medications");
 
 const profileStyle = {
-    textAlign: "Center",
+    textAlign: "center",
     width: "50%",
     margin: "auto",
     marginTop: "20px",
-    marginBottom: "20px",
+    marginBottom: "40px",
     fontFamily: "inherit",
     fontSize: "25px",
     boxSizing: "inherit",
@@ -38,7 +31,7 @@ const profileStyle = {
 const medStyle = {
   position: 'relative',
   float: 'left',
-  textAlign: "Center",
+  textAlign: "center",
     margin: "auto",
     fontFamily: "inherit",
     fontSize: "25px",
@@ -63,8 +56,63 @@ const Profile = () => {
 
   const [name, setName] = useState('')
   const [emailAddress, setEmailAddress] = useState('')
-  const [userMeds, setMedications] = useState([{}])
-  const [userApps, setAppointments] = useState([{}])
+  const [gender, setGender] = useState('')
+
+  const [updatedFirstName, setFirstName] = useState('')
+  const [updatedLastName, setLastName] = useState('')
+  const [updatedEmail, setUpdatedEmail] = useState('')
+  const [updatedGender, setUpdatedGender] = useState('')
+
+
+  const updateUserInfo = () => {
+
+    onAuthStateChanged(auth, (user)=>{
+      if(user)
+      {
+        const updateUserDoc = async(loggedInUser) =>
+        {
+          const userDoc = doc(db, "Profiles", loggedInUser.uid)
+        
+          if(updatedFirstName !== "")
+          {
+            await updateDoc(userDoc, {
+              fName: updatedFirstName
+            })
+          }
+
+          if(updatedLastName !== "")
+          {
+            await updateDoc(userDoc, {
+              lName: updatedLastName
+            })
+          }
+
+          const emailRegex = new RegExp("[a-z0-9]@[a-z].[a-z]", "i")
+
+          if(updatedEmail !== "")
+          {
+            if ((emailRegex.test(updatedEmail)===true))
+            {
+              await updateDoc(userDoc, {
+                email: updatedEmail
+              })
+            }
+          }
+
+          if(updatedGender !== "")
+          {
+            await updateDoc(userDoc, {
+              sex: updatedGender
+            })
+          }
+
+          window.location.reload(true)
+        }
+
+        updateUserDoc(user)
+      } 
+    })
+  }
 
   const getUserInfo = async(loggedInUser) => {
     const userQuery = query(users, where('uid', '==', loggedInUser.uid))
@@ -76,98 +124,18 @@ const Profile = () => {
       setName(fullName)
       let email = doc.data().email
       setEmailAddress(email)
+      let sex = doc.data().sex
+      setGender(sex)
     })
   }
   
-  const getMedInfo = async(loggedInUser) => {
-    const medQuery = query(medications, where('profileID', '==', '/Profiles/' + loggedInUser.uid))
-    const medSnapshot = await getDocs(medQuery)
-    
-    let i = 0;
-    var meds = []
-    medSnapshot.forEach((doc)=>{
-      console.log(doc.data())
-      let name = doc.data().name
-      let startDate = doc.data().time.toDate()
-      let amount = doc.data().amount
-      let days = doc.data().days
-      var daysToTake = ""
-
-      if (days.Mon === true)
-      {
-        daysToTake+="Monday\n"
-      }
-
-      if (days.Tue === true)
-      {
-        daysToTake+="Tuesday\n"
-      }
-
-      if (days.Wed === true)
-      {
-        daysToTake+="Wednesday\n"
-      }
-
-      if (days.Thu === true)
-      {
-        daysToTake+="Thursday\n"
-      }
-
-      if (days.Fri === true)
-      {
-        daysToTake+="Friday\n"
-      }
-
-      if (days.Sat === true)
-      {
-        daysToTake+="Saturday\n"
-      }
-
-      if (days.Sun === true)
-      {
-        daysToTake+="Sunday"
-      }
-      
-      let id = i
-      let med = {id, name, startDate, amount, daysToTake}
-      meds[i] = med;
-      i++;
-      //userMedications.push(med)
-    })
-
-    // if(meds.length>0)
-    //     setMedications(...userMeds, meds)
-  }
   
-  const getAppointmentInfo = async(loggedInUser) => {
-    const appQuery = query(appointments, where('profileID', '==', '/Profiles/' + loggedInUser.uid))
-    var apps = []
-    const appSnapshot = await getDocs(appQuery)
-    let i = 0;
-    appSnapshot.forEach((doc)=>{
-      console.log(doc.data())
-      let name = doc.data().name
-      let date = doc.data().appointmentDate.toDate()
-      let address = doc.data().address
-      let notes = doc.data().notes
-      let id = i
-
-      let app={id, name, date, address, notes}
-      apps[i] = app
-      //userAppointments.push(app)
-      i++;
-     
-    })
-
-  }
   
   onAuthStateChanged(auth, (user)=>{
    
     if(user)
     {
       getUserInfo(user)
-      getAppointmentInfo(user)
-      getMedInfo(user)
     }
     
     else
@@ -180,47 +148,44 @@ const Profile = () => {
           <Card sx={profileStyle} variant="outlined">
             <CardHeader title='PROFILE INFORMATION' sx={{textDecoration: 'underline'}}></CardHeader>
             <Divider></Divider>
-            <CardContent>Username: {name}</CardContent>
-            <Divider></Divider>
-            <CardContent>Email: {emailAddress}</CardContent>
-            <Divider></Divider>
-            <CardContent><TexttoSpeech id={0} textToRead={"Profile Information, UserName, " + name + " , Email, " + emailAddress} icon="Info"></TexttoSpeech></CardContent>
-            <Divider></Divider>
+              <CardContent>Username: {name}</CardContent>
+              <Divider></Divider>
+              <CardContent>Email: {emailAddress}</CardContent>
+              <Divider></Divider>
+              <CardContent>Gender: {gender}</CardContent>
+              <Divider></Divider>
+              <CardContent><TexttoSpeech id={0} textToRead={"Profile Information, UserName, " + name + " , Email, " + emailAddress + " , Gender, " + gender} icon="Info"></TexttoSpeech></CardContent>
+              <Divider></Divider>
           </Card>
 
-          <Card sx={medStyle}>
-            <CardHeader title="MEDICATIONS" sx={{textDecoration: 'underline'}}></CardHeader>
+          <Card sx={profileStyle} variant="outlined">
+            <CardHeader title='EDIT INFORMATION' sx={{textDecoration: 'underline'}}></CardHeader>
             <Divider></Divider>
-            
-              {userMeds.map((meds, index) => {
-                
-                return(
-                <div key={index}>
-                    <div>Name: {meds.name}</div>
-                    <div>Start Date: {meds.startDate}</div>
-                    <div>Amount: {meds.amount}</div>
-                    <TexttoSpeech id={index} icon="Volume" textToRead={"Name, " + meds.name + ", Start Date, " + meds.startDate + ", Amount to take, " + meds.amount}></TexttoSpeech>
-                    <Divider></Divider>
-               </div>)
-})}
+            {/* <FormControl> */}
+                <CardContent>Edit First Name: <Input type="text" onChange={(event)=>setFirstName(event.target.value)} disableUnderline={true}></Input> </CardContent>
+                <Divider variant="fullWidth"></Divider>
+                <CardContent>Edit Last Name: <Input type="text" onChange={(event)=>setLastName(event.target.value)} disableUnderline={true}></Input> </CardContent>
+                <Divider></Divider>
+                {/* <CardContent>Edit Password: <Input type="text" disableUnderline={true}></Input> </CardContent>
+                <Divider></Divider> */}
+                <CardContent>Edit E-mail: <Input type="text" onChange={(event)=>setUpdatedEmail(event.target.value)} disableUnderline={true}></Input> </CardContent>
+                <Divider></Divider>
+                <CardContent>Edit Gender: 
+                  <RadioGroup onChange={(event)=>setUpdatedGender(event.target.value)} row={true} sx={{alignItems: 'center', justifyContent: 'center'}}>
+                    <FormControlLabel value="female" control={<Radio />} label="Female"></FormControlLabel>
+                    <FormControlLabel value="male" control={<Radio />} label="Male"></FormControlLabel>
+                    <FormControlLabel value="undetermined" control={<Radio />} label="Prefer Not to Say"></FormControlLabel>
+                  </RadioGroup>
+                </CardContent>
+                <Divider></Divider>
+                <CardContent>
+                  <Button onClick={updateUserInfo} variant='contained' color='success' sx={{fontSize: 'inherit', border: '1px solid black', fontFamily: 'inherit', borderRadius: '25px'}}>Submit</Button>
+                </CardContent>
+            {/* </FormControl> */}
           </Card>
 
-          <Card sx={appStyle}>
-            <CardHeader title="APPOINTMENTS" sx={{textDecoration: 'underline'}}></CardHeader>
-              <Divider></Divider>
-          {userApps.map((apps, index) => {
-            
-            return (
-            <div key={index}>
-              <div>Name: {apps.name}</div>
-              <div>Date: {apps.date}</div>
-              <div>Address: {apps.address} </div>
-              <div>Notes: {apps.notes}</div>
-              <TexttoSpeech id={index} icon="Volume" textToRead={"Name, " + apps.name + ", Date, " + apps.date + ", Address, " + apps.address + ", Notes, " + apps.notes}></TexttoSpeech>
-              <Divider></Divider>
-            </div>
-          )})}
-          </Card>
+
+
     </>)
   
 }
