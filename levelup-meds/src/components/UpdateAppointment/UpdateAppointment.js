7a7c319cd291d3e714d392a0777db31f872b5b23
raops01 <raops01@pfw.edu>
2023-03-14 23:39:18 -0400

    changed db import

--- levelup-meds/src/components/UpdateAppointment/UpdateAppointment.js
+++ levelup-meds/src/components/UpdateAppointment/UpdateAppointment.js
@@ -1,5 +1,5 @@
 import {useRef, useState} from "react";
-import { db } from "../firebase/config";
+import db from "../database/FirestoreConfig";
 import { collection, getDocs, updateDoc, doc, Timestamp, query, where } from "firebase/firestore";
 import Select from "react-select";
 import auth from "../Auth/AuthProvider";
